2e2d7ceb0157175a845de52bd5ab8dc5bcfe30d9
zhongch4g <zhongch4all@gmail.com>
2017-05-02 16:26:13 +0800

    update 70 & Unfinished 53

---
+++ LeetCode/53. Maximum Subarray.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8 -*-
+"""
+    53. Maximum Subarray
+    Directed by user zhongch4g
+    current system date 2017/5/2
+"""
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 贪心算法 & DP
---
+++ LeetCode/70. Climbing Stairs.py
@@ -0,0 +1,22 @@
+#-*-coding:utf-8 -*-
+"""
+    70. Climbing Stairs
+    Directed by user zhongch4g
+    current system date 2017/5/2
+"""
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n-1)+self.climbStairs(n-2)
+
+
+
+instance = Solution()
+print instance.climbStairs(15)
