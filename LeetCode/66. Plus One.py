31119e9757d89e869cfe19607ba14fede89ae47b
zhongch4g <zhongch4all@gmail.com>
2017-05-05 23:34:16 +0800

    null

---
+++ LeetCode/66. Plus One.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8 -*-
+"""
+    66. Plus One
+    Directed by user zhongch4g
+    current system date 2017/5/5
+"""
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # 今天刷本子
