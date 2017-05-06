7cb217e575969421055e2c9da21f0d3783905708
zhongch4g <zhongch4all@gmail.com>
2017-05-06 23:22:52 +0800

    update 345 ac version

--- LeetCode/345. Reverse Vowels of a String.py
+++ LeetCode/345. Reverse Vowels of a String.py
@@ -11,33 +11,40 @@ class Solution(object):
         :rtype: str
         """
         # 最后一个用例超时
-        # vowels_str = ""
-        # for chars in s:
-        #     if chars in "aeiouAEIOU":
-        #         vowels_str += chars
-        # print vowels_str
-        # i = 0
+        # new version
+        vowels_str = ""
+        for chars in s:
+            if chars in "aeiouAEIOU":
+                vowels_str += chars
+        i = 0
         # reverse = ""
-        # vowels_str_re = vowels_str[::-1]
-        # for j in range(len(s)):
-        #     if s[j] in "aeiouAEIOU":
-        #         reverse += vowels_str_re[i]
-        #         i += 1
-        #     if s[j] not in "aeiouAEIOU":
-        #         reverse += s[j]
+        vowels_str_re = vowels_str[::-1]
+        print vowels_str_re
+        list_s = list(s)
+        for j in range(len(list_s)):
+            if list_s[j] in "aeiouAEIOU":
+                # reverse += vowels_str_re[i]
+                # i += 1
+                list_s[j] = vowels_str_re[i]
+                i += 1
+            # else:
+            #     reverse += s[j]
         # return reverse
-        sa = [x for x in s]
-        st, end = 0, len(sa) - 1
-        vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
-        while st < end:
-            while st < end and sa[st] not in vowels:
-                st += 1
-            while st < end and sa[end] not in vowels:
-                end -= 1
-            if st != end:
-                sa[st], sa[end] = sa[end], sa[st]
-            st, end = st+1, end-1
-        return "".join(sa)
+        # print list_s
+        return "".join(list_s)
+
+        # sa = [x for x in s]
+        # st, end = 0, len(sa) - 1
+        # vowels = set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
+        # while st < end:
+        #     while st < end and sa[st] not in vowels:
+        #         st += 1
+        #     while st < end and sa[end] not in vowels:
+        #         end -= 1
+        #     if st != end:
+        #         sa[st], sa[end] = sa[end], sa[st]
+        #     st, end = st+1, end-1
+        # return "".join(sa)
 
 instance = Solution()
 print instance.reverseVowels("hello")
