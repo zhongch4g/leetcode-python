2b2d126affbb391e01facce1c8ae5664ec65a1ee
zhongch4g <zhongch4all@gmail.com>
2017-05-06 22:54:02 +0800

    update 367 new version

--- LeetCode/367. Valid Perfect Square.py
+++ LeetCode/367. Valid Perfect Square.py
@@ -10,3 +10,29 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        # 1 4 16 25 36
+        # 对于一个整数 n，它的平方写成 n2。n2等于头 n 个正奇数的和
+
+        # l, r=1, num
+        #
+        # while l<=r:
+        #     m= (l+r)/2
+        #     if m*m== num:
+        #         return True
+        #     elif m*m<num:
+        #         l=m+1
+        #     else:
+        #         r=m-1
+        # return False
+
+        n = 1
+        while n*n<=num:
+            n += 1
+        if n**2 == num or (n-1)**2 == num:
+            return True
+        else:
+            return False
+
+
+instance = Solution()
+print instance.isPerfectSquare(25)
