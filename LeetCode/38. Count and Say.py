c9fee36ae7215de473cde3fabac518ca1ba6b1de
zhongch4g <zhongch4all@gmail.com>
2017-05-11 22:02:30 +0800

    update Unfinished 38.

---
+++ LeetCode/38. Count and Say.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8 -*-
+"""
+    38. Count and Say
+    Directed by user zhongch4g
+    current system date 2017/5/11
+"""
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
