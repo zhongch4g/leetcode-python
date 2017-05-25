e0bc222b720be6d471defe50c3da916686823c64
zhongch4g <zhongch4all@gmail.com>
2017-05-25 22:47:16 +0800

    add 400.

---
+++ LeetCode/400. Nth Digit.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8 -*-
+"""
+    400. Nth Digit
+    Directed by user zhongch4g
+    current system date 2017/5/25
+"""
+class Solution(object):
+    def findNthDigit(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
