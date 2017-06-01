ec56ce00361f3575d6767c0f340e42a1a7e9bfd7
zhongch4g <zhongch4all@gmail.com>
2017-06-01 21:08:35 +0800

    458.

--- LeetCode/458. Poor Pigs.py
+++ LeetCode/458. Poor Pigs.py
@@ -4,3 +4,11 @@
     Directed by user zhongch4g
     current system date 2017/5/31
 """
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
