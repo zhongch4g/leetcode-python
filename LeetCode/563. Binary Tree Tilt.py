e6434c15d1c7113e4561f3ed444b4df6c9e4a605
zhongch4g <zhongch4all@gmail.com>
2017-11-01 15:42:19 +0800

    453.reback 563.new

---
+++ LeetCode/563. Binary Tree Tilt.py
@@ -0,0 +1,21 @@
+#-*-coding:utf-8 -*-
+"""
+    # @File    : 563. Binary Tree Tilt.py
+    # @Author  : zhongch4g
+    # @Time    : 2017/11/1 15:41
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
