c3c8e1d5ea59325984a0efbdb06397d3702b630a
zhongch4g <zhongch4all@gmail.com>
2017-11-03 11:24:42 +0800

    206.

--- LeetCode/206. Reverse Linked List.py
+++ LeetCode/206. Reverse Linked List.py
@@ -30,3 +30,61 @@ class Solution(object):
         #     i += 1
         # return head
 
+        # 反转单链表(O(N) 空间复杂度)
+        nodeList = []
+        if head is None:
+            return None
+        while head is not None:
+            nodeList.append(head.val)
+            head = head.next
+        nodeList.reverse()
+        newHead = ListNode(nodeList[0])
+        rhead = newHead
+        for val in nodeList[1:]:
+            node = ListNode(val)
+            newHead.next = node
+            newHead = newHead.next
+        return rhead
+
+    def reverseList1(self, head):
+        cur = head
+        nex = head
+        pre = None
+        while cur is not None:
+            nex = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nex
+        return pre
+
+    def reverseList2(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if not head or not head.next:
+            return head
+
+        p, q = head, head.next
+        while q:
+            temp = q.next
+            q.next = p
+            p, q = q, temp
+
+        head.next = None
+
+        return p
+
+
+    def reverseList4(self, head):
+        cur = head
+        nex = head
+        pre = None
+        while cur is not None:
+            nex = cur.next
+            cur.next = pre
+
+            pre = cur
+            cur = nex
+        return pre
