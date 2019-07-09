12087d60267e40d7096c8ffb37439b0cfc45d7dc
zhongch4g <zhongch4all@gmail.com>
2019-07-09 13:58:46 -0500

    leave

---
+++ LeetCode/6. ZigZag Conversion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/9 1:55 PM
+# @Author  : zhongch4g
+# @Site    : 
+# @File    : 6. ZigZag Conversion.py
+# @Software: IntelliJ IDEA
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
