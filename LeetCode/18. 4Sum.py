ebd7e0b22c3b7909b5ac443bf41e0849c6ee7e09
zhongch4g <zhongch4all@gmail.com>
2019-07-11 11:56:35 -0500

    search pruning

--- LeetCode/18. 4Sum.py
+++ LeetCode/18. 4Sum.py
@@ -5,3 +5,50 @@
 # @Site    : 
 # @File    : 18. 4Sum.py
 # @Software: IntelliJ IDEA
+
+
+class Solution:
+    def fourSum(self, nums, target):
+        if len(nums) < 4:
+            return []
+        nums.sort()
+        res = []
+        max_num = nums[-1]
+        for i in range(len(nums)-3):
+            a = nums[i]
+            ### Attention
+            if a > 0 and a >= target:
+                break
+            if 4 * a > target:
+                break
+            if a + 3 * max_num < target:
+                continue
+            ### End Attention...
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            for j in range(i + 1, len(nums)-2):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue
+                b = nums[j]
+
+                l, r = j + 1, len(nums) - 1
+                while l < r:
+                    cur = a + b + nums[l] + nums[r]
+                    if cur == target:
+                        res.append([a, b, nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r + 1]:
+                            r -= 1
+                    elif cur > target:
+                        r -= 1
+                    else:
+                        l += 1
+        print(res)
+
+
+solution = Solution()
+solution.fourSum([0,0,0,0], 0)
