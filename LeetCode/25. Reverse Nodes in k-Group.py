7bcedcf0a3351f459bdb7e4eb2e234262459c4e2
zhongch4g <zhongch4all@gmail.com>
2019-07-12 19:53:37 -0500

    F

---
+++ LeetCode/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/12 5:20 PM
+# @Author  : zhongch4g
+# @Site    : 
+# @File    : 25. Reverse Nodes in k-Group.py
+# @Software: IntelliJ IDEA
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseKGroup(self, head, k):
+        pass
