b18684f0f3f2feba4a26e742b1971348f6baacf7
zhongch4g <zhongch4all@gmail.com>
2019-08-21 21:00:26 -0500

    quick select

--- LeetCode/973. K Closest Points to Origin.py
+++ LeetCode/973. K Closest Points to Origin.py
@@ -21,3 +21,37 @@ class Solution:
                 heapq.heappush(heap, (-distance, (x, y)))
 
         return [t[1] for t in heap]
+
+
+    # quick select
+
+    def kClosest2(self, points, K: int):
+
+        length =  len(points)
+        l = 0
+        r = length - 1
+        while l <= r:
+            mid = self.helper(points, l, r)
+            if mid == K:
+                break
+            if mid < K:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return points[:K]
+
+    def helper(self, A, l, r):
+        pivot = A[l]
+        while (l < r):
+            while l < r and self.compare(A[r], pivot) >= 0:
+                r -= 1
+            A[l] = A[r]
+            while l < r and self.compare(A[l], pivot) <= 0:
+                l += 1
+            A[r] = A[l]
+        A[l] = pivot
+        return l
+
+    def compare(self, p1, p2):
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]
