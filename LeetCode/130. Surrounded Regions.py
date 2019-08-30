c19620838daeb386853912ffa173dc115583fae7
zhongch4g <zhongch4all@gmail.com>
2019-08-30 15:26:04 -0500

    Union Find Solution

--- LeetCode/130. Surrounded Regions.py
+++ LeetCode/130. Surrounded Regions.py
@@ -56,3 +56,55 @@ class Solution:
                     board[i][j] = 'X'
                 if board[i][j] == 'W':
                     board[i][j] = 'O'
+
+
+# Union Find Solution
+class Solution2:
+    def solve(self, board) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board or len(board) == 0:
+            return
+
+        m, n = len(board), len(board[0])
+
+        dummy = m * n
+        uf = UF(m, n)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    cvt = i * n + j
+                    if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                        uf.union(cvt, dummy)
+                        continue
+                    for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                        nx = i + x
+                        ny = j + y
+                        if nx < 0 or nx >= m or ny < 0 or ny >= n or board[nx][ny] != 'O':
+                            continue
+                        neighbor = nx * n + ny
+                        uf.union(neighbor, cvt)
+        for i in range(m):
+            for j in range(n):
+                cid = i * n + j
+                if board[i][j] == 'O' and uf.find(cid) != uf.find(dummy):
+                    board[i][j] = 'X'
+
+class UF:
+    def __init__(self, m, n):
+        self.father = [i for i in range(m * n + 1)]
+
+
+    def union(self, id1, id2):
+        root_id1 = self.find(id1)
+        root_id2 = self.find(id2)
+        if root_id1 != root_id2:
+            self.father[root_id1] = root_id2
+
+    def find(self, cid):
+        if cid == self.father[cid]:
+            return cid
+        self.father[cid] = self.find(self.father[cid])
+        return self.father[cid]
