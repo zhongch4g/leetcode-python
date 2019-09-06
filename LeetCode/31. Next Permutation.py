2eedfda8831473719c32f326229ca92b27561b2e
zhongch4g <zhongch4all@gmail.com>
2019-09-05 21:10:28 -0500

    add solution

--- LeetCode/31. Next Permutation.py
+++ LeetCode/31. Next Permutation.py
@@ -25,3 +25,26 @@ class Solution:
             nums[i], nums[j] = nums[j], nums[i]
         # step3: reverse from i + 1 to the end
         nums[i+1:] = nums[i+1:][::-1]
+
+
+    def nextPermutation2(self, nums) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) <= 1:
+            return nums
+        i = len(nums) - 1
+        # find the first increasing number
+        while i > 0 and nums[i - 1] >= nums[i]:
+            i -= 1
+
+        # reverse from increasing number to the end
+        nums[i:] = reversed(nums[i:])
+        print(nums, i - 1, i)
+        # from reversed numbers find the first number that bigger than increasing number
+        if i > 0:
+            pivot = i - 1
+            while i < len(nums) and nums[i] <= nums[pivot]:
+                i += 1
+            nums[pivot], nums[i] = nums[i], nums[pivot]
+        return nums
